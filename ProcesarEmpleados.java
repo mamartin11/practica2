3351cdc85d1bc34ea7ee967d84def22b443aacd5
mamartin11 <111911651+mamartin11@users.noreply.github.com>
2022-11-04 17:59:10 -0500

    Add files via upload
    Práctica #2 -> Programación funcional
    Miguel Ángel Martínez 
    Camilo Soto

---
+++ ProcesarEmpleados.java
@@ -0,0 +1,141 @@
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class ProcesarEmpleados {
+
+    static List<Empleado> empleados;
+
+    static void cargarArchivo() throws IOException{
+        Pattern pattern = Pattern.compile(";");
+        String fileName = "empleado.csv";
+        try (Stream<String> lines = Files.lines(Path.of(fileName))){
+            empleados = lines.map(line -> {
+                String[] arr = pattern.split(line);
+                return new Empleado(arr[0], arr[1], arr[2],Double.parseDouble(arr[3]), arr[4] );
+            }).collect(Collectors.toList());
+        }
+    }
+
+    static Predicate<Empleado> Rsalario=
+            e -> (e.getSalario() >= 4000 && e.getSalario() <= 6000);
+    static void mostrarEmpleadosRango() {
+        System.out.println("LOS EMPLEADOS QUE GANAN ENTRE $4000-$6000 SON: ");
+        empleados.stream()
+                .filter(Rsalario)
+                .sorted(Comparator.comparing(Empleado::getSalario))
+                .forEach(System.out::println);
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+    }
+
+    static void empleadosDepartamento(){
+        System.out.println("LOS EMPLEADOS POR DEPARTAMENTO SON:");
+        Map<String, List<Empleado>> agrupadoPorDepartamento =
+                empleados.stream()
+                        .collect(Collectors.groupingBy(Empleado::getDepartamento));
+        agrupadoPorDepartamento.forEach(
+                (departamento, empleadosEnDepartamento) ->
+                {
+                    System.out.println(departamento);
+                    empleadosEnDepartamento.forEach(
+                            empleado -> System.out.printf(" %s%n", empleado));
+                }
+        );
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+    }
+
+    static void cantidadEmpleadosDepartamento(){
+        System.out.println("LA CANTIDAD DE EMPLEADOS POR DEPARTAMENTO ES: ");
+        Map<String, Long> conteoEmpleadosPorDepartamento =
+                empleados.stream()
+                        .collect(Collectors.groupingBy(Empleado::getDepartamento,
+                                TreeMap::new, Collectors.counting()));
+        conteoEmpleadosPorDepartamento.forEach(
+                (departamento, conteo) -> System.out.printf(
+                        "%s tiene %d empleado(s)%n", departamento, conteo));
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+
+    }
+    static void sumaNominaDepartamento(){
+        System.out.println("La sumatoria de nóminas por departamento son: ");
+    }
+
+
+    static void empleadoGanaMasDepartamento(){
+        System.out.println("Los EMPLEADOS QUE GANAN MÁS DE CADA DEPARTAMENTO SON: ");
+
+    }
+    static Predicate<Empleado> Maxsalario=
+            e -> (e.getSalario() == empleados.stream().mapToDouble(Empleado::getSalario).max().getAsDouble());
+    static void empleadoGanaMas(){
+
+        System.out.println("EL EMPLEADO QUE GANA MÁS ES: ");
+                empleados.stream().filter(Maxsalario).forEach(System.out::println);
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+
+    }
+    static Predicate<Empleado> Minsalario=
+            e -> (e.getSalario() == empleados.stream().mapToDouble(Empleado::getSalario).min().getAsDouble());
+
+    static void empleadoGanaMenos(){
+        System.out.println("EL EMPLEADO QUE GANA MENOS ES: ");
+        empleados.stream().filter(Minsalario).forEach(System.out::println);
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+
+    }
+
+    static void promediosalarioDepartamento(){
+
+        System.out.println("La sumatoria de nóminas por departamento son: ");
+    }
+
+    static void promedioSalario(){
+        System.out.printf("Promedio de salarios de los empleados: %.2f%n",
+                empleados.stream()
+                        .mapToDouble(Empleado::getSalario)
+                        .average()
+                        .getAsDouble());
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+
+    }
+
+    static void nomina(){
+        System.out.printf("La nómina total es: %.2f%n",
+                empleados.stream()
+                        .mapToDouble(Empleado::getSalario)
+                        .sum());
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+
+    }
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        cargarArchivo();
+        mostrarEmpleadosRango();
+        empleadosDepartamento();
+        cantidadEmpleadosDepartamento();
+        //
+        //
+        sumaNominaDepartamento();
+        empleadoGanaMas();
+        empleadoGanaMenos();
+        promedioSalario();
+        nomina();
+    }
+
+}
